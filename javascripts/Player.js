6e3154fdceb97835a3af2e41086f355f493e52e8
Arkadiusz Abramowicz <areek99@gmail.com>
2021-12-15 21:52:25 +0100

    Zmienie obszaru zakresu ciala na okragly

--- javascripts/Player.js
+++ javascripts/Player.js
@@ -8,6 +8,17 @@ export class Player extends Phaser.Physics.Matter.Sprite{
     } = data
     super(scene.matter.world, x,y, texture,frame)
     this.scene.add.existing(this);
+
+    const{Body, Bodies} = Phaser.Physics.Matter.Matter;
+
+    let playerCollider = Bodies.circle(this.x,this.y,12,{isSensor:false, label:"playerCollider"})
+    let playerSensor = Bodies.circle(this.x, this.y, 24, {isSensor:true, label:"playerSensor"})
+    const compoundBody = Body.create({
+      parts:[playerCollider, playerSensor],
+      frictionAir:0.35,
+    })
+    this.setExistingBody(compoundBody)
+    this.setFixedRotation();
   }
 
   //Podczas tworzenia sceny załaduj odpowiednie assety jako sprite dla danej postaci
